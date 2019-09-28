6a5c9dd987707f68f9d3d81e9e93eb37cf14dbbc
Peter Toth <tothpeter08@gmail.com>
2019-09-28 16:29:30 +0200

    Implement Sum#plus

--- tests/main/AllTests.java
+++ tests/main/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BankTest.class, MoneyTest.class, IntegrationTest.class })
+@SuiteClasses({ BankTest.class, MoneyTest.class, SumTest.class, IntegrationTest.class })
 public class AllTests {
 
 }
---
+++ tests/main/SumTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SumTest {
+
+	@Test
+	public void testSumPlusMoney() {
+		Expression fiveBucks = Money.dollar(5);
+		Expression tenFrancs = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+		Money result = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(15), result);
+	}
+
+}
