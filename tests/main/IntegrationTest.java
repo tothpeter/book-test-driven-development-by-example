d68bf30c7dd1246b1a8e6409d676f4bb17550e6e
Peter Toth <tothpeter08@gmail.com>
2019-09-28 15:56:30 +0200

    Basic implementation of final add for different currrencies

---
+++ tests/main/IntegrationTest.java
@@ -0,0 +1,19 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+	@Test
+	public void testMixedAddition() {
+		Money fiveBucks = Money.dollar(5);
+		Money tenFrancs = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+		assertEquals(Money.dollar(10), result);
+	}
+
+}
