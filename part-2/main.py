9f9e6ddf2284bf5c4d70b44544f7302383fc3ef4
Peter Toth <tothpeter08@gmail.com>
2019-09-29 18:18:10 +0200

    Invoke tearDown afterward

--- part-2/main.py
+++ part-2/main.py
@@ -4,6 +4,9 @@ class WasRun(TestCase):
     def setUp(self):
         self.log = 'setUp '
 
+    def tearDown(self):
+        self.log = self.log + 'tearDown '
+
     def testMethod(self):
         self.log = self.log + 'testMethod '
 
@@ -11,6 +14,6 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun('testMethod')
         test.run()
-        assert(test.log == 'setUp testMethod ')
+        assert(test.log == 'setUp testMethod tearDown ')
 
 TestCaseTest('testTemplateMethod').run()
