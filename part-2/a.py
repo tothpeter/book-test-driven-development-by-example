62fedbdb6b4bee9a6bdc81845eb7d17d90d1ef64
Peter Toth <tothpeter08@gmail.com>
2019-09-29 17:00:19 +0200

    First hardwired test

---
+++ part-2/a.py
@@ -0,0 +1,15 @@
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+
+    def testMethod(self):
+        self.wasRun = 1
+
+    def run(self):
+        self.testMethod()
+
+
+test = WasRun("testMethod")
+print test.wasRun
+test.run()
+print test.wasRun
