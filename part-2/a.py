297ab1a803e88015c2aeec60b505d11f0ce73e23
Peter Toth <tothpeter08@gmail.com>
2019-09-29 17:00:55 +0200

    Call test methods dynamically

--- part-2/a.py
+++ part-2/a.py
@@ -1,15 +1,17 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
     def testMethod(self):
         self.wasRun = 1
 
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
 
 
-test = WasRun("testMethod")
+test = WasRun('testMethod')
 print test.wasRun
 test.run()
 print test.wasRun
