8091aa4ac79f6e6b1f64f290b4f43f868c375704
Peter Toth <tothpeter08@gmail.com>
2019-09-29 17:53:11 +0200

    Invoke setUp first

--- part-2/main.py
+++ part-2/main.py
@@ -1,18 +1,24 @@
 from test_case import *
 
 class WasRun(TestCase):
-    def __init__(self, name):
+    def setUp(self):
         self.wasRun = None
-        TestCase.__init__(self, name)
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun('testMethod')
+
     def testRunning(self):
-        test = WasRun('testMethod')
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest('testRunning').run()
+TestCaseTest('testSetUp').run()
