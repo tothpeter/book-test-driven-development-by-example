5f434c6c68f6247b6ece862b72f508c02653b88e
Peter Toth <tothpeter08@gmail.com>
2019-09-29 18:13:56 +0200

    Use log field to follow what happened in what order

--- part-2/main.py
+++ part-2/main.py
@@ -2,23 +2,15 @@ from test_case import *
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = 'setUp '
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + 'testMethod '
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun('testMethod')
+    def testTemplateMethod(self):
+        test = WasRun('testMethod')
+        test.run()
+        assert(test.log == 'setUp testMethod ')
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
-TestCaseTest('testRunning').run()
-TestCaseTest('testSetUp').run()
+TestCaseTest('testTemplateMethod').run()
