c1993d59ed98cbf7e1738e2b8fc882b1e2c8c12e
Peter Toth <tothpeter08@gmail.com>
2019-09-29 17:01:28 +0200

    Introduce the TestCase super class and move the run method to it

--- part-2/a.py
+++ part-2/a.py
@@ -1,14 +1,18 @@
-class WasRun:
+class TestCase:
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
-
-    def testMethod(self):
-        self.wasRun = 1
 
     def run(self):
         method = getattr(self, self.name)
         method()
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
 
 
 test = WasRun('testMethod')
