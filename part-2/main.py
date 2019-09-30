895b6401783459bf9eea910c5f171e48e509c141
Peter Toth <tothpeter08@gmail.com>
2019-09-30 09:45:09 +0200

    Add basic result test reporter

--- part-2/main.py
+++ part-2/main.py
@@ -16,4 +16,10 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.log == 'setUp testMethod tearDown ')
 
+    def testResult(self):
+        test = WasRun('testMethod')
+        result = test.run()
+        assert(result.summary() == '1 run, 0 failed')
+
 TestCaseTest('testTemplateMethod').run()
+TestCaseTest('testResult').run()
